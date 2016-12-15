e9adcce51068fc333a40d6e306aeeb5dcd7a3b7b
Jon Brede Skaug <jbs@ramenstudios.com>
2016-12-15 14:02:35 +0100

    Testing new code if game object id is not found

--- main.js
+++ main.js
@@ -669,7 +669,9 @@ function doTask(creep, task, params){
         break;
         case REPAIR_TASK:
         if (creep.carry == 0) findTask(creep);
-
+        if (targetGameobj == null) {
+            targetGameobj = new RoomPosition(creep.memory.target.pos.x, creep.memory.target.pos.y, creep.memory.target.pos.roomName).lookFor(LOOK_STRUCTURES)[0];
+        }
         var repairAttempt = creep.repair(targetGameobj);
             if(repairAttempt == ERR_NOT_IN_RANGE) {
                 doTask(creep, MOVETO);
