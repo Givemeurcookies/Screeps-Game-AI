db6b7bfd877438bd78447469500378a8959b7dbf
Jon Brede Skaug <jbs@ramenstudios.com>
2016-12-15 12:56:25 +0100

    Updating path creation

--- main.js
+++ main.js
@@ -438,12 +438,18 @@ function setTask(creep, task, params){
         if(debug.creeps) console.log(creep.name+": Trying to expand...");
         var closestSpawn   = creep.pos.findClosestByRange(FIND_MY_SPAWNS),
             sources        = creep.room.find(FIND_SOURCES),
+            storage        = room.find(FIND_MY_STRUCTURES, {filter:function(structure){
+                return structure.structureType == 'STRUCTURE_STORAGE' || structure.structureType == 'STRUCTURE_CONTAINER'
+            }}),
             roomController = [creep.room.controller],
             keyPaths       = [],
             keyPoints      = [];
 
         if(roomController != undefined) {
             keyPoints = sources.concat(roomController);
+            if (storage){
+                keyPoints = keyPoints.concat(storage);
+            }
         } else {
             keyPoints = sources;
         }
