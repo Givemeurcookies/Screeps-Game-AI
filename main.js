cd6c0059bd9d92e65b2c540bfe3aef95efd2858f
Jon Brede Skaug <jbs@ramenstudios.com>
2016-12-16 11:12:27 +0100

    Logging transfer

--- main.js
+++ main.js
@@ -644,8 +644,10 @@ function doTask(creep, task, params){
                     if(!isCreepNextToSource(closecreep) && _.sum(closecreep.carry) != closecreep.carryCapacity) transferTarget.push(closecreep);
                 });
             } else {
+
                 var isNextToLink = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {filter: (structure) => {return structure.structureType == STRUCTURE_LINK}});
                 if(isNextToLink.length > 0){
+                    console.log(creep.name+" is next to link, transferring into link");
                     transferTarget.push(isNextToLink[0]);
                 }
             }
