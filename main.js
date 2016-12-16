1fd452485938522004bc298e1c9a6d16361f7d31
Jon Brede Skaug <jbs@ramenstudios.com>
2016-12-16 10:36:22 +0100

    Might be a validation error

--- main.js
+++ main.js
@@ -483,7 +483,7 @@ function setTask(creep, task, params){
         var closestSpawn   = creep.pos.findClosestByRange(FIND_MY_SPAWNS),
             sources        = creep.room.find(FIND_SOURCES),
             storage        = creep.room.find(FIND_MY_STRUCTURES, {filter:function(structure){
-                return structure.structureType == 'STRUCTURE_STORAGE' || structure.structureType == 'STRUCTURE_CONTAINER'
+                return (structure.structureType == 'STRUCTURE_STORAGE' || structure.structureType == 'STRUCTURE_CONTAINER')
             }}),
             roomController = [creep.room.controller],
             keyPaths       = [],
