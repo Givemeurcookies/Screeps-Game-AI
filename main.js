b5b5bdc5fa9352ba5d7be29be0dd7074c24844ba
Jon Brede Skaug <jbs@ramenstudios.com>
2017-01-07 06:20:38 +0100

    Dropping lodash solution for native solution

--- main.js
+++ main.js
@@ -11,5 +11,9 @@ module.exports.loop = function(){
     for(var id in Game.spawns) Game.spawns[id].run();
     for(var name in Game.creeps)  Game.creeps[name].run();
     console.log(Object.prototype.toString.call(Memory.constants.actions));
-    console.log(_.findKey(Memory.constants.actions, [31]));
+    console.log(findKey(Memory.constants.actions, 31));
+}
+
+function findKey(obj, value) {
+    return Object.keys(obj).filter(x => obj[x].includes(value))[0];
 }
