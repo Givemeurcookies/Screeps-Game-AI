72aaae03819ab8d27cd57fee645244e60d58eca0
Jon Brede Skaug <jbs@ramenstudios.com>
2017-01-07 06:07:09 +0100

    Checking what is saved in constants memory property

--- main.js
+++ main.js
@@ -10,5 +10,6 @@ module.exports.loop = function(){
 
     for(var id in Game.spawns) Game.spawns[id].run();
     for(var name in Game.creeps)  Game.creeps[name].run();
+    console.log(JSON.stringify(Memory.constants.actions));
     console.log(_.findKey(Memory.constants.actions, 30));
 };
