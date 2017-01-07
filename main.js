59c538fd0c784755170cb5f321605bba4f5c9af9
Jon Brede Skaug <jbs@ramenstudios.com>
2017-01-07 06:16:50 +0100

    Check object type

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ module.exports.loop = function(){
 
     for(var id in Game.spawns) Game.spawns[id].run();
     for(var name in Game.creeps)  Game.creeps[name].run();
-    console.log(Memory.constants.actions);
+    console.log(Object.prototype.toString.call(Memory.constants.actions));
     console.log(_.findKey(Memory.constants.actions, [31]));
 }
