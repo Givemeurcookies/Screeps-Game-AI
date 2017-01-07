f5b5e7611205848228abaf85214f381c9a214988
Jon Brede Skaug <jbs@ramenstudios.com>
2017-01-07 05:11:42 +0100

    Might have troubles with file permissions

--- main.js
+++ main.js
@@ -11,4 +11,4 @@ module.exports.loop = function(){
     console.log(taskcode_string[DROP]);
 };
 
-// Server, testing push on commit hook over terminal attempt 5
+// Server, testing push on commit hook over terminal attempt 6
