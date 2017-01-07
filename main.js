3de0573201520de1d3c3cc7243a9ab832f992231
Jon Brede Skaug <jbs@ramenstudios.com>
2017-01-07 05:17:01 +0100

    Local server now updates automatically on commit

--- main.js
+++ main.js
@@ -10,5 +10,3 @@ module.exports.loop = function(){
     for(var name in Game.creeps)  Game.creeps[name].run();
     console.log(taskcode_string[DROP]);
 };
-
-// Server, testing push on commit hook over terminal attempt 9
