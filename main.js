1a2afd932396aa5ae19fd7aa8b50e13a6f95e427
Jon Brede Skaug <jbs@ramenstudios.com>
2017-01-07 16:27:48 +0100

    Adding sources to cache per room

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ require('creep');
 
 module.exports.loop = function(){
     Object.assign(global, Memory.constants.actions, Memory.constants.tasks);
-
+    var taskGivers = [];
     for(var id in Game.spawns) Game.spawns[id].run();
     for(var name in Game.creeps) Game.creeps[name].run();
 
     for(var roomName in Game.rooms) {
         var room = Game.rooms[roomName];
-        console.log(JSON.stringify('Sources: '+room.find(FIND_SOURCES)));
-
+        if(!room.memory.sources) room.memory.sources = room.find(FIND_SOURCES));
+        for(let sourceid in room.memory.sources) taskGivers.push(room.memory.sources[sourceid]);
     }
 
-    for(var taskGiver in Memory.taskGivers){
-
+    for(var taskGiver in taskGivers){
+        console.log(taskGivers[taskGiver]);
     }
 
 }
