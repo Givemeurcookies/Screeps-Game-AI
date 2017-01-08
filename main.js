2328ee3f8a35f47045d78d5f0270896b55ed4ddb
Jon Brede Skaug <jbs@ramenstudios.com>
2017-01-08 04:21:56 +0100

    Set upgrading

--- main.js
+++ main.js
@@ -32,40 +32,50 @@ module.exports.loop = function(){
         // Let's keep this so we can see what giver that's requesting whatever
         console.log(colorText('purple',JSON.stringify(giver.pos)));
         console.log(giver instanceof Source);
-        // Returns total and available
-        var sourceAccess =  findAccessibleTiles(giver.room,
-                            giver.pos.x-1, giver.pos.y-1,
-                            giver.pos.x+1, giver.pos.y+1);
+        if(giver instanceof Source){
+            // Returns total and available
+            var sourceAccess =  findAccessibleTiles(giver.room,
+                                giver.pos.x-1, giver.pos.y-1,
+                                giver.pos.x+1, giver.pos.y+1);
 
-        if(sourceAccess.available > 0){
-            // Request creep to harvest
-            if(creepsInRoom.length > 0){
-                // Go over creeps in the room
-                // to see if any are available
-                // and fit for task
-                var creep = giver.pos.findClosestByRange(creepsInRoom, {filter:function(creep){ return !creep.memory.task.busy}});
-                // If any creeps is availabe, otherwise don't do anything
-                if(creep) {
-                    console.log(colorText('orange', 'Found creep :: '+creep.name+'Trying to set task from giver'));
-                    creep.set({taskCode:ACTION_HARVEST, target:giver});
-                }
-            } else if(true) {
-                // We set this value to true until we get a better system
-                // Request a new creep to be spawned
-                var spawns = [];
-                for(var i in Game.spawns){
-                    spawns.push(Game.spawns[i]);
-                }
-                console.log(colorText('blue', giver.pos.findClosestByRange(spawns)));
-                giver.pos.findClosestByRange(spawns).requestCreep([WORK, MOVE, CARRY, MOVE], {
-                    task:{
-                        busy   : false,
-                        target : false,
-                        params : false
+            if(sourceAccess.available > 0){
+                // Request creep to harvest
+                if(creepsInRoom.length > 0){
+                    // Go over creeps in the room
+                    // to see if any are available
+                    // and fit for task
+                    var creep = giver.pos.findClosestByRange(creepsInRoom, {filter:function(creep){ return !creep.memory.task.busy}});
+                    // If any creeps is availabe, otherwise don't do anything
+                    if(creep) {
+                        console.log(colorText('orange', 'Found creep :: '+creep.name+'Trying to set task from giver'));
+                        creep.set({taskCode:ACTION_HARVEST, target:giver});
                     }
-                });
+                } else if(true) {
+                    // We set this value to true until we get a better system
+                    // Request a new creep to be spawned
+                    var spawns = [];
+                    for(var i in Game.spawns){
+                        spawns.push(Game.spawns[i]);
+                    }
+                    console.log(colorText('blue', giver.pos.findClosestByRange(spawns)));
+                    giver.pos.findClosestByRange(spawns).requestCreep([WORK, MOVE, CARRY, MOVE], {
+                        task:{
+                            busy   : false,
+                            target : false,
+                            params : false
+                        }
+                    });
+                }
+            }
+        } else if(giver instanceof StructureController){
+            var creep = giver.pos.findClosestByRange(creepsInRoom, {filter:function(creep){ return !creep.memory.task.busy}});
+            // If any creeps is availabe, otherwise don't do anything
+            if(creep) {
+                console.log(colorText('orange', 'Found creep :: '+creep.name+' trying to set task from giver'));
+                creep.set({taskCode:ACTION_UPGRADE, target:giver});
             }
         }
+
 
     }
     // Go through spawns and creeps!
